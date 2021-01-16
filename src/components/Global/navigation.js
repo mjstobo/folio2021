a9da0deaca0c1d48fdb3419e4abe9b08469097a8
mjstobo <mjstobo@gmail.com>
2021-01-16 18:19:26 +1100

    main

--- src/components/Global/navigation.js
+++ src/components/Global/navigation.js
@@ -1,20 +1,35 @@
 import React from "react"
-import navigationStyles from './navigation.module.css';
+import navigationStyles from "./navigation.module.css"
 
 export default function Navigation() {
   return (
-  <nav className={navigationStyles.nav}>
-     <span className={navigationStyles.wordmark}>Matthew Stobo</span>
+    <nav className={navigationStyles.nav}>
+      <span className={navigationStyles.wordmark}>Matthew Stobo</span>
       <ul className={navigationStyles.navlist}>
-          <li className={navigationStyles.item}>
-              Work
-          </li>
-          <li className={navigationStyles.item}>
-              Skills & Experience
-          </li>
-          <li className={navigationStyles.item}>
-              Contact
-          </li>
+        <li className={navigationStyles.item}>
+          <a
+            href="https://www.github.com/mjstobo"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>
+        </li>
+        <li className={navigationStyles.item}>
+          <a
+            href="https://www.github.com/mjstobo"
+            target="_blank"
+            rel="noreferrer"
+          >
+            LinkedIn
+          </a>
+        </li>
+        <li className={navigationStyles.item}>
+          <a href="mailto: mjstobo@gmail.com" target="_blank" rel="noreferrer">
+            Contact Me
+          </a>
+        </li>
       </ul>
-  </nav>)
+    </nav>
+  )
 }
